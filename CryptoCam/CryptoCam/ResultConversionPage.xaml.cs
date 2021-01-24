4e4cb2a39ae1ef59b46384c54d35e16f423b06cc
Juan Sparisci <juan_sparisci@hotmail.com>
2021-01-25 00:13:19 +1100

    *Some code refactoring *New page and viewmodel to show the modal with the result

---
+++ CryptoCam/CryptoCam/ResultConversionPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using CryptoCam.Model;
+using CryptoCam.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CryptoCam
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ResultConversionPage : ContentPage
+    {
+        public ResultConversionPage(System.IO.Stream stream, FiatCurrency selectedFiatCurrency, CryptoCurrency selectedCryptoCurrency)
+        {
+            InitializeComponent();
+            BindingContext = new ResultConversionPageViewModel(stream,selectedFiatCurrency,selectedCryptoCurrency);
+        }
+    }
+}
---
+++ CryptoCam/CryptoCam/ViewModel/ResultConversionPageViewModel.cs
@@ -0,0 +1,40 @@
+﻿using CryptoCam.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace CryptoCam.ViewModel
+{
+    public class ResultConversionPageViewModel : INotifyPropertyChanged
+    {
+        private Stream imgStream;
+        private FiatCurrency selectedFiatCurrency;
+        private CryptoCurrency selectedCryptoCurrency;
+        private ImageSource focusImgSource;
+        public ImageSource FocusImgSource { get => focusImgSource; set { focusImgSource = value; OnPropertyChanged(); } }
+
+        public ResultConversionPageViewModel(Stream imgStream, FiatCurrency selectedFiatCurrency, CryptoCurrency selectedCryptoCurrency)
+        {
+            this.imgStream = imgStream;
+            FocusImgSource = ImageSource.FromStream(()=>imgStream);
+            this.selectedFiatCurrency = selectedFiatCurrency;
+            this.selectedCryptoCurrency = selectedCryptoCurrency;
+            CloseCommand = new Command(async () =>
+            {
+                await Application.Current.MainPage.Navigation.PopModalAsync();
+            }, () => { return true; });
+
+        }
+        public ICommand CloseCommand { protected set; get; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
