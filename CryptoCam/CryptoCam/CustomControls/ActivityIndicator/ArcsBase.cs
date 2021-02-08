eebece910d286a8fc711fa2107168847c7c7c4b6
Juan Sparisci <juan_sparisci@hotmail.com>
2021-02-08 18:21:06 +1100

    * Reusable customized activities indicator created

---
+++ CryptoCam/CryptoCam/CustomControls/ActivityIndicator/ArcsBase.cs
@@ -0,0 +1,30 @@
+﻿using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CryptoCam.CustomControls.ActivityIndicator
+{
+    public abstract class ArcsBase : ContentView, IArcs
+    {
+
+        internal SKCanvasView canvas { get; set; }
+        internal Stopwatch stopwatch { get; set; }
+        internal float OvalStartAngle { get; set; }
+        internal float SecondOvalStartAngle { get; set; }
+        internal float OvalSweepAngle { get; set; } 
+        internal float InnerOvalStartAngle { get; set; }
+        internal float InnerSecondOvalStartAngle { get; set; }
+        internal float InnerOvalSweepAngle { get; set; }
+
+        internal SKPaint firstArcPaint { get; set; }
+        internal SKPaint secondArcPaint { get; set; }
+
+        public abstract void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args);
+
+        public abstract bool OnTimerClik();
+    }
+}
